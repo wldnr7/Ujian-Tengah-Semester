ca5d0ab4e6a5a75ef758918ef09cfd27b293dab7
Wildan Nur Rahman <80866806+wldnr7@users.noreply.github.com>
2021-04-22 09:22:57 +0700

    Add files via upload

---
+++ TugasProgramNo2/Person.java
@@ -0,0 +1,23 @@
+package com.java.TugasProgramNo2;
+
+public class Person {
+
+    String name;
+    String email;
+    String country;
+    String phoneNumber;
+    int age;
+    String address;
+
+    void showPerson(){
+
+        System.out.println("Name         : " + name);
+        System.out.println("Age          : " + age + " years old");
+        System.out.println("Country      : " + country);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Email        : " + email);
+        System.out.println("Address      : " + address);
+        System.out.println("\n");
+    }
+
+}
